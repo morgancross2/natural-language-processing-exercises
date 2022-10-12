5c6b9e3433910fd15b3233cf93a2027c02734148
Morgan Cross <morgan.e.cross2@gmail.com>
2022-10-12 16:57:15 -0500

    added notes from review

--- acquire.py
+++ acquire.py
@@ -2,36 +2,34 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-def get_blog_article_content(art):
-    output = {}
-    output['headline'] = art.find('h2').text.strip()
-    output['posted'] = art.find_all('p')[0].text.strip()
-    output['content'] = art.find_all('p')[1].text.strip()
-    return output
+def get_blog_article_content(url, header = {'User-Agent': 'Codeup Data Science'}):
+    soup = BeautifulSoup(requests.get(url, headers=header).content, 'html.parser')
+    return [link['href'] for link in soup.select('a.more-link')]
 
-def get_blog_articles(url):
-    headers = {'User-Agent': 'Codeup Data Science'}
-    soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
-    arts = soup.select('article')
-    final = [get_blog_article_content(art) for art in arts]
-    return pd.DataFrame(final)
+def get_blog_articles(base_url, header = {'User-Agent': 'Codeup Data Science'}):
+    urls = get_blog_article_content(base_url)
+    output = []
+    for blog in urls:
+        article_soup = BeautifulSoup(requests.get(blog, headers=header).content)
+        blog_output = {'title': article_soup.select_one('h1.entry-title').text,
+        'content': article_soup.select_one('div.entry-content').text.strip()}
+        output.append(blog_output)
+    return pd.DataFrame(output)
 
-def get_news_article_content(url, page):
-    temp = {}
-    soup = BeautifulSoup(requests.get(url+page).content, 'html.parser')
-    headers = soup.select('a.clickable')
-    contents = soup.select('div.news-card-content.news-right-box')
-    titles = [title.span.text.strip() for title in headers]
-    arts = [art.div.text.strip() for art in contents]
-    temp['title'] = titles
-    temp['content'] = arts
-    temp['category'] = page.strip('/')
-    return pd.DataFrame(temp)
+def get_cats(url):
+    soup = BeautifulSoup(requests.get(url).content)
+    return [cat.text.lower() for cat in soup.find_all('li')[1:]]
 
 def get_news_articles(url):
-    pages = ['/business', '/sports', '/technology', '/entertainment']
-    results = pd.DataFrame()
-    for page in pages:
-        hold = get_news_article_content(url, page)
-        results = pd.concat([results, hold])
-    return results
+    cats = get_cats(url)
+    output = []
+    for cat in cats:
+        cat_url = url + '/' + cat
+        cat_soup = BeautifulSoup(requests.get(cat_url).content)
+        cat_titles = [title.text for title in cat_soup.find_all('span', itemprop='headline')]
+        cat_bodies = [body.text for body in cat_soup.find_all('div', itemprop='articleBody')]
+        caticles = [{'title': title,
+                     'content': body,
+                     'category': cat} for title, body in zip(cat_titles, cat_bodies)]
+        output.extend(caticles)
+    return pd.DataFrame(output)
